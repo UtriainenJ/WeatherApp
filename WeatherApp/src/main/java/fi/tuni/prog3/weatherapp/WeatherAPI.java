60af140675a799757e8f54cac864ae91eae5bd7b
Jerri Tarpio <jerri.tarpio@tuni.fi>
2024-04-11 16:54:35 +0300

    create getCurrentWeather(d, d)

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/WeatherAPI.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/WeatherAPI.java
@@ -3,6 +3,7 @@ package fi.tuni.prog3.weatherapp;
 import com.google.gson.JsonParser;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Locale;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -29,48 +30,39 @@ public class WeatherAPI implements iAPI {
     
     private static String makeHTTPCall(String url) throws IOException {
         OkHttpClient client = new OkHttpClient();
-
-            Request request = new Request.Builder()
-                    .url(url)
-                    .build();
-            
-            try {
-                Response response = client.newCall(request).execute();
-
-                // Check if the response is successful
-                if (response.isSuccessful()) {
-                    return response.body().string();
-                }
-            } catch (IOException ex) {
-                System.out.println("Error getting api response.");
+        Request request = new Request.Builder().url(url).build();
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                return response.body().string();
             }
+        } catch (IOException ex) {
+            System.out.println("Error getting api response.");
+        }
         return null;
     }
 
     @Override
     public String lookUpLocation(String loc) {
         try {
-            //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key};
-
             String key = getAPIKey();
-            String city = loc;
-            
-            String url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+key;
-            
-            String res = makeHTTPCall(url);
-            return res;
-            
-        } catch (IOException ex) {
-            // api file not found failure
-        }
-        
+            String url = "https://api.openweathermap.org/data/2.5/weather"
+                    + "?q=" + loc + "&appid=" + key;
+            return makeHTTPCall(url);
+        } catch (IOException ex) {}
         return null;
     }
 
     @Override
     public String getCurrentWeather(double lat, double lon) {
-        //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-        
+        try {
+            String key = getAPIKey();
+            String latStr = String.format(Locale.US,"%.2f", lat);
+            String lonStr = String.format(Locale.US,"%.2f", lon);
+            String url = "https://api.openweathermap.org/data/2.5/weather"
+                    + "?lat=" + latStr + "&lon=" + lonStr + "&appid=" + key;
+            return makeHTTPCall(url);
+        } catch (IOException ex) {}
         return null;
     }
 
